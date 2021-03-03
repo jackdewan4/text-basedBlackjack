2b2038cd6dcc137ef48a9b7585af71afa8fe940a
jackdewan <jdraider15@gmail.com>
2021-03-02 20:41:11 -0500

    interactive version

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -5,15 +5,16 @@ public class BlackJack {
     Hand dHand = new Hand();
     Scanner in = new Scanner(System.in);
 
-    public void reset(boolean reshuffle){
+    public void reset(boolean reshuffle) {
 
-        if(reshuffle == true){
+        if (reshuffle) {
             deck.Build();
             deck.Shuffle();
         }
     }
-    public void deal(){
-        // deals two cards into both hands
+
+    public void deal() {
+        // deals two cards into both player and dealer hands
         deck.Deal();
         pHand.add(deck.top);
         deck.Deal();
@@ -24,24 +25,25 @@ public class BlackJack {
         dHand.add(deck.top);
     }
 
-    public void playerTurn(){
+    public void playerTurn() {
 
         char response;
-        while(pHand.getTotalValue() < 16){
-            //System.out.println("Would you like to hit?(y/n)");
-            //response = in.next().charAt(0);
-            //if(response == 'y' || response == 'Y'){
+        while (pHand.getTotalValue() < 16) {
+            System.out.println("Would you like to hit?(y/n)");
+            response = in.next().charAt(0);
+            if (response == 'y' || response == 'Y') {
                 deck.Deal();
                 pHand.add(deck.top);
-                //System.out.println(pHand.toString());
-                //System.out.println( "Your hand has total of: " + pHand.getTotalValue());
+                System.out.println(pHand.toString());
+                System.out.println("and has value of: " + pHand.getTotalValue());
 
-            //}else{
-                //break;
+            } else {
+
+                break;
             }
-        System.out.println("Player's hand is: " + pHand.getTotalValue());
+            System.out.println("Player's hand is: " + pHand.getTotalValue());
 
-        //}
+            }
         /*if(pHand.getTotalValue() > 21){
             System.out.println("Bust!!");
         }else if(pHand.getTotalValue() == 21){
@@ -49,16 +51,15 @@ public class BlackJack {
         }*/
 
 
-
-    }
-
-    public void dealerTurn(){
-        while(dHand.getTotalValue() < 17){
-            deck.Deal();
-            dHand.add(deck.top);
-
         }
-        System.out.println("Dealer's hand is: " + dHand.getTotalValue());
+
+        public void dealerTurn () {
+            while (dHand.getTotalValue() < 17) {
+                deck.Deal();
+                dHand.add(deck.top);
+
+            }
+            System.out.println("Dealer's hand is: " + dHand.getTotalValue());
 
         /*if(dHand.getTotalValue()> 21){
             System.out.println("Dealer bust.");
@@ -67,70 +68,73 @@ public class BlackJack {
         }*/
 
 
-    }
+        }
 
-    public static void main(String [] args){
+        public static void main (String[]args){
 
-        char answer = 'y';
-        System.out.println("Welcome to BlackJack");
-        BlackJack game = new BlackJack();
+            char answer = 'y';
+            System.out.println("Welcome to BlackJack");
+            BlackJack game = new BlackJack();
 
-        for(int i = 0; i < 25; i++) {
+            //for (int i = 0; i < 25; i++) {
 
-            System.out.println("Welcome to a new round of BlackJack:");
-            //while(answer == 'y' || answer == 'Y'){
-            // sets up game by building a deck of 52 cards and shuffling it.
-            if (game.deck.deck.size() < 20) {
-                game.reset(true);
-            }
-            game.deal();
+                //System.out.println("Welcome to a new round of BlackJack:");
 
-                /*System.out.println(game.pHand.toString());
-                System.out.println(game.pHand.getTotalValue());*/
-            // run player turn
-            game.playerTurn();
-            // run dealer turn
-            game.dealerTurn();
+                while(answer == 'y' || answer == 'Y'){
+                // sets up game by building a deck of 52 cards and shuffling it.
+                if (game.deck.deck.size() < 20) {
+                    game.reset(true);
+                }
+                game.deal();
 
-            if (game.pHand.getTotalValue() < 21 && game.dHand.getTotalValue() < 21) {
+                System.out.println(game.pHand.toString());
+                System.out.println("and has value of: " + game.pHand.getTotalValue());
 
-                if (game.pHand.getTotalValue() > game.dHand.getTotalValue()) {
-                    System.out.println("Player wins!!");
+                // run player turn
+                game.playerTurn();
+                // run dealer turn
+                game.dealerTurn();
 
-                } else if (game.pHand.getTotalValue() < game.dHand.getTotalValue()) {
-                    System.out.println("Dealer wins!!");
+                // check only if player and dealer have a hand less than 21
+                if (game.pHand.getTotalValue() < 21 && game.dHand.getTotalValue() < 21) {
+
+                    if (game.pHand.getTotalValue() > game.dHand.getTotalValue()) {
+                        System.out.println("Player wins!!");
+
+                    } else if (game.pHand.getTotalValue() < game.dHand.getTotalValue()) {
+                        System.out.println("Dealer wins!!");
+                    }
                 }
 
-            }
+                if (game.pHand.getTotalValue() > 21 && game.dHand.getTotalValue() > 21) {
+                    System.out.println("Player and dealer both bust, push!");
+                } else if (game.pHand.getTotalValue() == 21 && game.dHand.getTotalValue() == 21) {
+                    System.out.println("Player and dealer both have blackjack, push!");
+                } else if (game.pHand.getTotalValue() < 21 && game.dHand.getTotalValue() > 21) {
+                    System.out.println("Player wins! Dealer bust!");
+                } else if (game.dHand.getTotalValue() < 21 && game.pHand.getTotalValue() > 21) {
+                    System.out.println("Dealer wins! Player bust!");
+                } else if (game.dHand.getTotalValue() == game.pHand.getTotalValue()) {
+                    System.out.println("Player and dealer have the same value hand, push!");
+                } else if (game.dHand.getTotalValue() == 21) {
+                    System.out.println("Dealer has blackjack");
+                } else if (game.pHand.getTotalValue() == 21) {
+                    System.out.println("Player has blackjack");
+                }
+
+                // resets hands back to 0 at end of game
+                game.pHand.reset();
+                game.dHand.reset();
+
+                System.out.println( "\n" +"Would you like to play another game?(y/n)");
+                answer = game.in.next().charAt(0);
+                if(answer == 'n' || answer == 'N'){
+                System.out.println("Thanks for playing BlackJack today!!");
 
 
-            if (game.pHand.getTotalValue() > 21 && game.dHand.getTotalValue() > 21) {
-                System.out.println("Player and dealer both bust, push!");
-            } else if (game.pHand.getTotalValue() == 21 && game.dHand.getTotalValue() == 21) {
-                System.out.println("Player and dealer both have blackjack, push!");
-            } else if (game.pHand.getTotalValue() < 21 && game.dHand.getTotalValue() > 21) {
-                System.out.println("Player wins! Dealer bust!");
-            } else if (game.dHand.getTotalValue() < 21 && game.pHand.getTotalValue() > 21) {
-                System.out.println("Dealer wins! Player bust!");
-            } else if (game.dHand.getTotalValue() == game.pHand.getTotalValue()) {
-                System.out.println("Player and dealer have the same value hand, push!");
-            } else if (game.dHand.getTotalValue() == 21) {
-                System.out.println("Dealer has blackjack");
-            } else if (game.pHand.getTotalValue() == 21) {
-                System.out.println("Player has blackjack");
-            }
+                }
 
-
-            game.pHand.reset();
-            game.dHand.reset();
-            //System.out.println( "\n" +"Would you like to play another game?(y/n)");
-            //answer = game.in.next().charAt(0);
-            //if(answer == 'n' || answer == 'N'){
-            //System.out.println("Thanks for playing BlackJack today!!");
-
-
-            //}
-
-        }// }
+            }// }
+        }
     }
-}
+
